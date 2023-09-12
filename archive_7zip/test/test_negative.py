ad2180cf53933dbee473822cb04360ac04602e0f
user <e.fomina.smr@mail.ru>
2023-09-12 22:41:24 +0300

    семинар

--- archive_7zip/test/test_negative.py
+++ archive_7zip/test/test_negative.py
@@ -1,14 +1,27 @@
+import pytest
+
 from checks import checkout_negativ
+import yaml
 
-folder_in = '/home/user/tst'
-folder_out = '/home/user/out'
-folder_ext = '/home/user/folder1'
-folder_bad = '/home/user/folder2'
+# folder_in = '/home/user/tst'
+# folder_out = '/home/user/out'
+# folder_ext = '/home/user/folder1'
+# folder_bad = '/home/user/folder2'
+
+with open('config.yaml') as fy:
+    data = yaml.safe_load(fy)
 
 
-def test_negative1():  # e извлекли из архива
-    assert checkout_negativ(f'cd {folder_bad}; 7z e arx2.7z -o{folder_ext} -y', "ERRORS"), "test1 FAIL"
+class TestNegative:
+    def test_negative1(self, make_folder, clear_folder, make_files, create_bad_archive):  # e извлекли из архива
+
+        assert checkout_negativ(f'cd {data["folder_bad"]}; 7z e arx2.7z -o{data["folder_ext"]} -y', "ERRORS")
+
+    def test_negative2(self, make_folder, clear_folder, make_files,
+                       create_bad_archive):  # t проверка целостности архива
+        assert checkout_negativ(f'cd {data["folder_bad"]}; 7z t arx2.7z', "Is not")
 
 
-def test_negative2():  # t проверка целостности архива
-    assert checkout_negativ(f'cd {folder_bad}; 7z t arx2.7z', "ERRORS"), 'test2 FAIL'
+
+if __name__ == '__main__':
+    pytest.main(['-vv'])
--- archive_7zip/test/test_positive.py
+++ archive_7zip/test/test_positive.py
@@ -1,139 +1,110 @@
 import random
 import string
+import yaml
 
-import pytest as pytest
 
 from checks import checkout, check_hash_crc32
 import pytest
 
-folder_in = '/home/user/tst'
-folder_out = '/home/user/out'
-folder_ext = '/home/user/folder1'
-folder_ext3 = '/home/user/folder3'
+with open('config.yaml') as fy:
+    data = yaml.safe_load(fy)
 
 
-@pytest.fixture()
-def make_folder():
-    return checkout(f'mkdir {folder_in} {folder_out} {folder_ext} {folder_ext3}', "")
+# folder_in = '/home/user/tst'
+# folder_out = '/home/user/out'
+# folder_ext = '/home/user/folder1'
+# folder_ext3 = '/home/user/folder3'
+
+class TestPositive:
+
+    def test_add_archive(self, make_folder, clear_folder, make_files):  # a создали архив
+        res_add = checkout(f'cd {data["folder_in"]}; 7z a {data["folder_out"]}/arx2', "Everything is Ok")
+        res_ls = checkout(f'ls {data["folder_out"]}', "arx2.7z")
+        assert res_add and res_ls
+
+    def test_check_e_extract(self, clear_folder, make_files):  #
+        res = list()
+        res.append(checkout(f'cd {data["folder_in"]}; 7z a {data["folder_out"]}/arx2', "Everything is Ok"))
+        res.append(checkout(f'cd {data["folder_out"]}; 7z e arx2.7z -o{data["folder_ext"]} -y', "Everything is Ok"))
+        for item in make_files:
+            res.append(checkout(f'ls {data["folder_ext"]}', item))
+
+        assert all(res)
+
+    def test_check_e_extract_subfolder(self, clear_folder, make_files, make_subfolder):
+        res = list()
+        res.append(checkout(f'cd {data["folder_in"]}; 7z a {data["folder_out"]}/arx2', "Everything is Ok"))
+        res.append(checkout(f'cd {data["folder_out"]}; 7z e arx2.7z -o{data["folder_ext"]} -y', "Everything is Ok"))
+        for item in make_files:
+            res.append(checkout(f'ls {data["folder_ext"]}', item))
+        for item in make_subfolder:
+            res.append(checkout(f'ls {data["folder_ext"]}', item))
+
+        assert all(res)
+
+    def test_check_x_extract_subfolder(self, clear_folder, make_files, make_subfolder):
+        # files, subflder and files in subfolder
+        res = list()
+        res.append(checkout(f'cd {data["folder_in"]}; 7z a {data["folder_out"]}/arx2', "Everything is Ok"))
+        res.append(checkout(f'cd {data["folder_out"]}; 7z x arx2.7z -o{data["folder_ext"]} -y', "Everything is Ok"))
+        for item in make_files:
+            res.append(checkout(f'ls {data["folder_ext"]}', item))
+
+        res.append(checkout(f'ls {data["folder_ext"]}', make_subfolder[0]))
+        res.append(checkout(f'ls {data["folder_ext"]}/{make_subfolder[0]}', make_subfolder[1]))
+
+        assert all(res)
+
+    def test_check_x_files(self, clear_folder, make_files):  # only files
+        res = list()
+        res.append(checkout(f'cd {data["folder_in"]}; 7z a {data["folder_out"]}/arx2', "Everything is Ok"))
+        res.append(checkout(f'cd {data["folder_out"]}; 7z x arx2.7z -o{data["folder_ext"]} -y', "Everything is Ok"))
+        for item in make_files:
+            res.append(checkout(f'ls {data["folder_ext"]}', item))
+        assert all(res)
+
+    def test_totality(self, clear_folder, make_files):  # t проверка целостности архива
+        res = list()
+        res.append(checkout(f'cd {data["folder_in"]}; 7z a {data["folder_out"]}/arx2', "Everything is Ok"))
+        res.append(checkout(f'cd {data["folder_out"]}; 7z t arx2.7z', "Everything is Ok"))
+
+        assert all(res)
+
+    def test_delete(self, clear_folder, make_files, make_subfolder):  # d удаление из архива
+        res = list()
+        res.append(checkout(f'cd {data["folder_in"]}; 7z a {data["folder_out"]}/arx2', "Everything is Ok"))
+        res.append(checkout(f'cd {data["folder_out"]}; 7z d arx2.7z', "Everything is Ok"))
+
+        assert all(res)
+
+    def test_update(self):  # u - обновление архива
+        assert checkout(f'cd {data["folder_in"]}; 7z u {data["folder_out"]}/arx2.7z', "Everything is Ok"), 'NO update'
+
+    # def test_check_archive(clear_folder, make_files):  #
+    #
+    #     result_add = checkout(f'cd {folder_in}; 7z a {folder_out}/arx2', "Everything is Ok")
+    #     result_check = checkout(f'ls {folder_out}', 'arx2.7z')
+    #     assert result_check and result_add, 'test_check_archive FAIL'
+
+    def test_nonempty_archive(self, clear_folder, make_files):
+        res = list()
+        res.append(checkout(f'cd {data["folder_in"]}; 7z a {data["folder_out"]}/arx2', "Everything is Ok"))
+        res.append(checkout(f'cd {data["folder_out"]}; 7z l arx2.7z', f'{len(make_files)} files'))
+
+    # def test_check_list_archive(clear_folder, make_files):
+    #     res = list()
+    #     res.append(checkout(f'cd {folder_in}; 7z a {folder_out}/arx2', "Everything is Ok"))
+    #
+    #     res.append(checkout(f'cd {folder_out}; 7z l arx2.7z', 'tst1')
+    #     res_tst2 = checkout(f'cd {folder_out}; 7z l arx2.7z', 'tst2')
+    #     assert res_tst1 and res_tst2, 'test_check_list_archive FAIL'
+
+    def test_check_hash(self):
+        hash_crc32 = check_hash_crc32(f'cd {data["folder_out"]}; crc32 arx2.7z')
+        res_upper = checkout(f'cd {data["folder_out"]}; 7z h arx2.7z', hash_crc32.upper())
+        res_lower = checkout(f'cd {data["folder_out"]}; 7z h arx2.7z', hash_crc32.lower())
+        assert res_lower or res_upper, 'NO equal hash'
 
 
-@pytest.fixture()
-def clear_folder():
-    return checkout(f'rm -rf {folder_in}/* {folder_out}/* {folder_ext}/* {folder_ext3}/*', "")
-
-
-@pytest.fixture()
-def make_files():
-    list_files = []
-    for i in range(5):
-        file_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
-        checkout(f'cd {folder_in}; dd if=/dev/urandom of={file_name} bs=512 count=1 iflag=fullblock', '')
-        list_files.append(file_name)
-
-    return list_files
-
-
-@pytest.fixture()
-def make_subfolder():
-    subfolder_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-    subfile_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
-    if not checkout(f'cd {folder_in}; mkdir {subfolder_name}', ''):
-        return None, None
-    if not checkout(f'cd {folder_in}/{subfolder_name}; '
-                    f'dd if=/dev/urandom of={subfile_name} bs=512 count=1 iflag=fullblock', ''):
-        return subfolder_name, None
-
-    return subfolder_name, subfile_name
-
-
-def test_add_archive(make_folder, clear_folder, make_files):  # a создали архив
-    res_add = checkout(f'cd {folder_in}; 7z a {folder_out}/arx2', "Everything is Ok")
-    res_ls = checkout(f'ls {folder_out}', "arx2.7z")
-    assert res_add and res_ls
-
-
-def test_check_e_extract(clear_folder, make_files):  #
-    res = list()
-    res.append(checkout(f'cd {folder_in}; 7z a {folder_out}/arx2', "Everything is Ok"))
-    res.append(checkout(f'cd {folder_out}; 7z e arx2.7z -o{folder_ext} -y', "Everything is Ok"))
-    for item in make_files:
-        res.append(checkout(f'ls {folder_ext}', item))
-
-    assert all(res)
-
-
-def test_check_e_extract_subfolder(clear_folder, make_files, make_subfolder):
-    res = list()
-    res.append(checkout(f'cd {folder_in}; 7z a {folder_out}/arx2', "Everything is Ok"))
-    res.append(checkout(f'cd {folder_out}; 7z e arx2.7z -o{folder_ext} -y', "Everything is Ok"))
-    for item in make_files:
-        res.append(checkout(f'ls {folder_ext}', item))
-    for item in make_subfolder:
-        res.append(checkout(f'ls {folder_ext}', item))
-
-    assert all(res)
-
-
-def test_check_x_extract_subfolder(clear_folder, make_files, make_subfolder):
-    # files, subflder and files in subfolder
-    res = list()
-    res.append(checkout(f'cd {folder_in}; 7z a {folder_out}/arx2', "Everything is Ok"))
-    res.append(checkout(f'cd {folder_out}; 7z x arx2.7z -o{folder_ext} -y', "Everything is Ok"))
-    for item in make_files:
-        res.append(checkout(f'ls {folder_ext}', item))
-
-    res.append(checkout(f'ls {folder_ext}', make_subfolder[0]))
-    res.append(checkout(f'ls {folder_ext}/{make_subfolder[0]}', make_subfolder[1]))
-
-    assert all(res)
-
-
-def test_check_x_files(clear_folder, make_files):  # only files
-    res = list()
-    res.append(checkout(f'cd {folder_in}; 7z a {folder_out}/arx2', "Everything is Ok"))
-    res.append(checkout(f'cd {folder_out}; 7z x arx2.7z -o{folder_ext} -y', "Everything is Ok"))
-    for item in make_files:
-        res.append(checkout(f'ls {folder_ext}', item))
-    assert all(res)
-
-
-def test_totality():  # t проверка целостности архива
-    assert checkout(f'cd {folder_out}; 7z t arx2.7z', "Everything is Ok"), 'test_totality FAIL'
-
-
-def test_delete():  # d удаление из архива
-    assert checkout(f'cd {folder_out}; 7z d arx2.7z', "Everything is Ok"), 'NO delete'
-
-
-def test_update():  # u - обновление архива
-    assert checkout(f'cd {folder_in}; 7z u {folder_out}/arx2.7z', "Everything is Ok"), 'NO update'
-
-
-def test_check_archive():  #
-    result_add = checkout(f'cd {folder_in}; 7z a {folder_out}/arx2', "Everything is Ok")
-    result_check = checkout(f'ls {folder_out}', 'arx2.7z')
-    assert result_check and result_add, 'test_check_archive FAIL'
-
-
-def test_check_extract():  # e извлекли из архива в заданную папку и ответили на все вопросы "да" -o задали директорию
-    result_ex = checkout(f'cd {folder_out}; 7z e arx2.7z -o{folder_ext} -y', "Everything is Ok"), 'test2 FAIL'
-    result_check1 = checkout(f'ls {folder_ext}', 'tst1')
-    result_check2 = checkout(f'ls {folder_ext}', 'tst2')
-    assert result_ex and result_check1 and result_check2, 'test_check_archive FAIL'
-
-
-def test_nonempty_archive():
-    assert checkout(f'cd {folder_out}; 7z l arx2.7z', '2 files'), 'test_check_archive FAIL'
-
-
-def test_check_list_archive():
-    res_tst1 = checkout(f'cd {folder_out}; 7z l arx2.7z', 'tst1')
-    res_tst2 = checkout(f'cd {folder_out}; 7z l arx2.7z', 'tst2')
-    assert res_tst1 and res_tst2, 'test_check_list_archive FAIL'
-
-
-def test_check_hash():
-    hash_crc32 = check_hash_crc32(f'cd {folder_out}; crc32 arx2.7z')
-    res_upper = checkout(f'cd {folder_out}; 7z h arx2.7z', hash_crc32.upper())
-    res_lower = checkout(f'cd {folder_out}; 7z h arx2.7z', hash_crc32.lower())
-    assert res_lower or res_upper, 'NO equal hash'
+if __name__ == '__main__':
+    pytest.main(['-vv'])
